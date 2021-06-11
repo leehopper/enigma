e175c409fa1e84a71541558aab0849b66aeded61
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-11 12:10:19 -0600

    Build test for new instance without date argument

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -9,20 +9,26 @@ describe Offset do
   end
 
   it 'attributes' do
-    offset = Offset.new('061121')
+    offset = Offset.new('110621')
 
-    expect(offset.date).to eq('061121')
+    expect(offset.date).to eq('110621')
+  end
+
+  it 'creates instance of self without date argument' do
+    offset = Offset.unknown
+
+    expect(offset).to be_a(Offset)
   end
 
   it '.translate_date' do
-    offset = Offset.new('061121')
+    offset = Offset.new('110621')
 
-    expect(offset.translate_date).to eq("3735776641")
+    expect(offset.translate_date).to eq("12237005641")
   end
 
   it '.create_shift_offset' do
-    offset = Offset.new('061121')
+    offset = Offset.new('110621')
 
-    expect(offset.create_shift_offset).to eq(['6', '6', '4', '1'])
+    expect(offset.create_shift_offset).to eq(['5', '6', '4', '1'])
   end
 end
