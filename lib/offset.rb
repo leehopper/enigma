517d9eeaefc5a0be84c9dc856b6020da0721f6ef
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-11 11:52:55 -0600

    Build method for create shift offset

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,6 +1,19 @@
 class Offset
+  attr_reader :date
 
   def initialize(date)
     @date = date
+  end
+
+  def translate_date
+    date_shift = (@date.to_i ** 2).to_s
+  end
+
+  def create_shift_offset
+    output = []
+    4.times do |acc|
+      output << translate_date[acc - 4]
+    end
+    output
   end
 end
