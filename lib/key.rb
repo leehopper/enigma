627dd9408ed7a9e6c16436326c7cff38f17b69c7
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-10 19:08:13 -0600

    Build methodf or create shift key

--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,18 @@
 class Key
 
   def initialize(key)
-    @key = key 
+    @key = key
+  end
+
+  def self.unknown
+    new(rand(0..99999).to_s)
+  end
+
+  def create_shift_key
+    output = []
+    4.times do |acc|
+      output << @key[acc] + @key[acc + 1]
+    end
+    output
   end
 end
