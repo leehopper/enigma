7d975b49e45d620d60aea7d4c7ccef9ea58bb322
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-11 11:52:38 -0600

    Build test for create shift offset

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -3,8 +3,20 @@ require_relative '../lib/offset'
 
 describe Offset do
   it 'exists' do
-    offset = Offset.new(061121)
+    offset = Offset.new('061121')
+
+    expect(offset).to be_a(Offset)
   end
 
+  it 'attributes' do
+    offset = Offset.new('061121')
 
+    expect(offset.date).to eq('061121')
+  end
+
+  it '.create_shift_offset' do
+    offset = Offset.new('061121')
+
+    expect(offset.create_shift_offset).to eq(['6', '6', '4', '1'])
+  end
 end
