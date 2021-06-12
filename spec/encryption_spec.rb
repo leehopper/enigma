64d3cbfbae107b9ca0426e3cdb7d1a7fa1b00849
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-12 14:18:01 -0600

    Build test for reduce shift and basic encryption

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -13,4 +13,17 @@ describe Encryption do
 
     expect(encryption.create_shift).to eq([6, 18, 27, 35])
   end
+
+  it '.reduce_shift' do
+    encryption = Encryption.new('Lee Jonathon Hopper', '01234', '110621')
+    expected = encryption.reduce_shift([6, 18, 27, 35])
+
+    expect(expected).to eq([6, 18, 0, 8])
+  end
+
+  it '.encrypt_character' do
+    encryption = Encryption.new('Lee Jonathon Hopper', '01234', '110621')
+
+    expect(encryption.encrypt_character('l', 6)).to eq('r')
+  end
 end
