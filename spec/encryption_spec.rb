b42697db1f1e9fbed47803aa391055495a32c28d
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-12 16:10:00 -0600

    Build test for encrypt text

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -23,8 +23,9 @@ describe Encryption do
 
   it '.split_and_downcase_text' do
     encryption = Encryption.new('Lee H', '01234', '110621')
+    encryption.split_and_downcase_text
 
-    expect(encryption.split_and_downcase_text).to eq(['l', 'e', 'e', ' ', 'h'])
+    expect(encryption.text).to eq(['l', 'e', 'e', ' ', 'h'])
   end
 
   it '.encrypt_character' do
@@ -32,5 +33,12 @@ describe Encryption do
 
     expect(encryption.encrypt_character('l', 6)).to eq('r')
     expect(encryption.encrypt_character('!', 6)).to eq('!')
+  end
+
+  it '.encrypt_text' do
+    encryption = Encryption.new('Lee Jonathon Hopper', '01234', '110621')
+    expected = encryption.encrypt_text([6, 18, 0, 8], ['l', 'e', 'e', ' ', 'h'])
+
+    expect(expected).to eq(['r', 'w', 'e', 'h', 'n'])
   end
 end
