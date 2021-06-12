fba30a705c44736a5498189bfb4168300b9073ea
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-11 18:22:22 -0600

    Build create shift method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -7,6 +7,21 @@ class Encryption
     @text = text
     @key = Key.new(key)
     @offset = Offset.new(date)
+    @shift = []
   end
 
+  def ceo
+    create_shift
+
+  end
+
+  def create_shift
+    acc = 0
+    @shift = []
+    @key.create_shift_key.each do |key|
+      @shift << key + @offset.create_shift_offset[acc]
+      acc += 1
+    end
+    @shift
+  end
 end
