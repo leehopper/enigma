1bed87d249e5aee5212756492fbb1655792e6a4f
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-11 18:22:11 -0600

    Build test for create shift

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -3,8 +3,14 @@ require_relative '../lib/encryption'
 
 describe Encryption do
   it 'exists' do
-    encryption = Encryption.new('Lee Jonathon Hopper', '01234', '061121')
+    encryption = Encryption.new('Lee Jonathon Hopper', '01234', '110621')
 
     expect(encryption).to be_a(Encryption)
+  end
+
+  it 'create_shift' do
+    encryption = Encryption.new('Lee Jonathon Hopper', '01234', '110621')
+
+    expect(encryption.create_shift).to eq([6, 18, 27, 35])
   end
 end
