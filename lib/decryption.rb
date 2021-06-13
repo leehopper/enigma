8ffddf1e71e3dc4032ba6eb37de6ef44d462c704
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-12 18:04:46 -0600

    Build methods for create shifts and split text

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -5,4 +5,32 @@ class Decryption
     @key = Key.new(key)
     @offset = Offset.new(date)
   end
+
+  def run_decryption
+    shift = create_shift
+    shift = reduce_shift(shift)
+    text_array = split_text
+    decrypted_text = encrypt_text(shift, text_array)
+    decrypted_text = format_text(decrypted_text)
+    create_output_hash(decrypted_text)
+  end
+
+  def create_shift
+    @key.create_shift_key.map.with_index do |key, index|
+      key + @offset.create_shift_offset[index]
+    end
+  end
+
+  def reduce_shift(shift)
+    shift.map do |num|
+      until num < 27
+        num -= 27
+      end
+      num
+    end
+  end
+
+  def split_text
+    temp_text = @text.split('')
+  end
 end
