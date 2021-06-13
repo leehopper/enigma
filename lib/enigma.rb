f2d88a94804da48f123af766266bddb55808d1bb
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-12 18:29:46 -0600

    Build decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,15 @@
 require_relative 'encryption'
+require_relative 'decryption'
 
 class Enigma
 
   def encrypt(text, key = 'none', date = 'none')
-    encription= Encryption.new(text, key, date)
+    encription = Encryption.new(text, key, date)
     encription.run_encryption
+  end
+
+  def decrypt(text, key, date)
+    decryption = Decryption.new(text, key, date)
+    decryption.run_decryption
   end
 end
