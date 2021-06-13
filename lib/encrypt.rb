5f9056e881713690a24701a3d44cd75d4d5a1f91
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-13 14:55:03 -0600

    Build command line file

---
+++ lib/encrypt.rb
@@ -0,0 +1,11 @@
+require_relative 'enigma'
+require 'date'
+
+file = File.open(ARGV[0], "r")
+message = file.read
+file.close
+enigma = Enigma.new
+encrypted = enigma.encrypt(message)
+writer = File.open(ARGV[1], "w")
+writer.write(encrypted[:encryption])
+puts "Created '#{ARGV[1]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
