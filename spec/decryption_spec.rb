44d0f0d9609b02ea3938da35caa4cad643bb1067
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-12 18:04:32 -0600

    Build tests for create shift and split text

--- spec/decryption_spec.rb
+++ spec/decryption_spec.rb
@@ -7,4 +7,23 @@ describe Decryption do
 
     expect(decryption).to be_a(Decryption)
   end
+
+  it 'create_shift' do
+    decryption = Decryption.new('rwehpfnizzovfzoxvwr', '01234', '110621')
+
+    expect(decryption.create_shift).to eq([6, 18, 27, 35])
+  end
+
+  it '.reduce_shift' do
+    decryption = Decryption.new('rwehpfnizzovfzoxvwr', '01234', '110621')
+    expected = decryption.reduce_shift([6, 18, 27, 35])
+
+    expect(expected).to eq([6, 18, 0, 8])
+  end
+
+  it '.split_and_downcase_text' do
+    decryption = Decryption.new('rwehp', '01234', '110621')
+
+    expect(decryption.split_text).to eq(['r', 'w', 'e', 'h', 'p'])
+  end
 end
