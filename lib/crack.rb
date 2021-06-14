7ab338d7579d096314f7783cb97947c7dea35ae0
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-14 16:07:04 -0600

    Build method for end array

--- lib/crack.rb
+++ lib/crack.rb
@@ -6,10 +6,21 @@ class Crack
 
   def run_cracking
     text_array = split_text
+    end_array = create_end_array(text_array)
   end
 
   def split_text
     @text.split('')
+  end
+
+  def create_end_array(text_array)
+    temp_array = text_array.push(:end)
+    end_index = temp_array.find_index(:end)
+    shift_array = [temp_array[end_index - 4]]
+    shift_array << [temp_array[end_index - 3]]
+    shift_array << [temp_array[end_index - 2]]
+    shift_array << [temp_array[end_index - 1]]
+    shift_array.flatten
   end
 
 end
