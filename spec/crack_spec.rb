d972d7b7f5fdbc63f76825838a714b9336df1cf3
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-14 16:44:14 -0600

    Build test for rotate alphabet and create shit

--- spec/crack_spec.rb
+++ spec/crack_spec.rb
@@ -20,4 +20,37 @@ describe Crack do
 
     expect(expected).to eq(['h', 's', 's', 'i'])
   end
+
+  it '.rotate_alphabet' do
+    crack = Crack.new('vjqtbeaweqihssi')
+
+    expected = crack.rotate_alphabet(0)
+    expect(expected[0]).to eq(' ')
+    expect(expected[26]).to eq('z')
+
+    expected = crack.rotate_alphabet(1)
+    expect(expected[0]).to eq('e')
+    expect(expected[26]).to eq('d')
+
+    expected = crack.rotate_alphabet(2)
+    expect(expected[0]).to eq('n')
+    expect(expected[26]).to eq('m')
+
+    expected = crack.rotate_alphabet(3)
+    expect(expected[0]).to eq('d')
+    expect(expected[26]).to eq('c')
+  end
+
+  it '.find_index_shift' do
+    crack = Crack.new('vjqtbeaweqihssi')
+
+    expect(crack.find_index_shift('h', 0)).to eq(8)
+    expect(crack.find_index_shift('i', 3)).to eq(5)
+  end
+
+  it '.create_shift' do
+    crack = Crack.new('vjqtbeaweqihssi')
+
+    expect(crack.create_shift(['h', 's', 's', 'i'])).to eq([8, 14, 5, 5])
+  end
 end
