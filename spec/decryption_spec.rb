86a79de53f5b5441089a6e0765595d5e0bb77a35
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-14 20:22:48 -0600

    Update tests for shift instance variable

--- spec/decryption_spec.rb
+++ spec/decryption_spec.rb
@@ -16,12 +16,13 @@ describe Decryption do
 
   it '.reduce_shift' do
     decryption = Decryption.new('rwehpfnizzovfzoxvwr', '01234', '110621')
-    expected = decryption.reduce_shift([6, 18, 27, 35])
+    decryption.create_shift
+    expected = decryption.reduce_shift
 
     expect(expected).to eq([6, 18, 0, 8])
   end
 
-  it '.split_and_downcase_text' do
+  it '.split_text' do
     decryption = Decryption.new('rwehp', '01234', '110621')
 
     expect(decryption.split_text).to eq(['r', 'w', 'e', 'h', 'p'])
@@ -29,7 +30,9 @@ describe Decryption do
 
   it '.decrypt_text' do
     decryption = Decryption.new('rwehp', '01234', '110621')
-    expected = decryption.decrypt_text([6, 18, 0, 8], ['r', 'w', 'e', 'h', 'p'])
+    decryption.create_shift
+    decryption.reduce_shift
+    expected = decryption.decrypt_text(['r', 'w', 'e', 'h', 'p'])
 
     expect(expected).to eq(['l', 'e', 'e', ' ', 'j'])
   end
@@ -73,5 +76,11 @@ describe Decryption do
     decryption = Decryption.with_cracked_shift('vjqtbeaweqihssi', [8, 14, 5, 5])
 
     expect(decryption.find_key_shift).to eq([3, 8, 1, 4])
+  end
+
+  it '.run_crack_decryption' do
+    decryption = Decryption.with_cracked_shift('vjqtbeaweqihssi', [8, 14, 5, 5])
+
+    expect(decryption.run_crack_decryption).to eq(0)
   end
 end
