5891efaac101c7805757fc9cb386874dd4c86072
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-14 19:39:31 -0600

    Build key shift method

--- lib/key.rb
+++ lib/key.rb
@@ -28,4 +28,71 @@ class Key
       key.to_i
     end
   end
+
+  def convert_key_shift(key_shift)
+    index1 = key_shift[0].to_s
+    index2 = key_shift[1].to_s
+    index3 = key_shift[2].to_s
+    index4 = key_shift[3].to_s
+    loop do
+      if index1.to_i < 9
+        index1 = '0' + index1
+      end
+      test1 = find_num(index1, index2)
+      if test1 == "restart"
+        reset = true
+      else
+        index2 = test1
+      end
+      unless reset == true
+        test2 = find_num(index2, index3)
+        if test2 == "restart"
+          reset = true
+        else
+          index3 = test2
+        end
+      end
+      unless reset == true
+        test3 = find_num(index3, index4)
+        if test3 == "restart"
+          reset = true
+        else
+          index4 = test3
+          break
+        end
+      end
+      if index1.to_i < 73
+        index1 = add_27(index1)
+      end
+      index2 = key_shift[1].to_s
+      index3 = key_shift[2].to_s
+      index4 = key_shift[3].to_s
+      reset = false
+    end
+    @key = index1 + index2[1] + index3[1] + index4[1]
+  end
+
+  def find_num(test1, test2)
+    num1 = test1
+    num2 = test2
+    restart = nil
+    until num1[1] == num2[0]
+      num2 = add_27(num2)
+      if num2.to_i > 99
+        restart = true
+        break
+      end
+    end
+    if restart == true
+      "restart"
+    else
+      num2
+    end
+  end
+
+  def add_27(num)
+    output = num.to_i
+    output += 27
+    output.to_s
+  end
 end
